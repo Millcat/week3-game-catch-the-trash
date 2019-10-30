272f8cb628e491f71500e9f90db313b62dfad89b
camille <camille.grison@gmail.com>
2019-10-30 13:21:15 +0100

    new instance turtle created + move back and forth + speed x3 for diver and turtle

--- src/js/items-objects.js
+++ src/js/items-objects.js
@@ -3,20 +3,25 @@ class Diver {
     this.x = x;
     this.y = y;
     this.domElement = domElement;
+    this.outLeft = false;
     // this.node = document.createElement("img"); // créer mon image dans mon HTML
     // document.getElementById("sea-screen").appendChild(this.node);
     // permet de localiser mon image dans mon HTML (dans la div parent)
   }
 
-  updateInDom() { // permet de mettre à jour la position du player dans le CSS
+  updateInDom() { // permet de mettre à jour la position du diver dans le CSS
     this.domElement.style.left = this.x + "px"; // l'inscrit dans le CSS, par exemple si this.x a bougé de 350 ==> left: "350px"
     this.domElement.style.top = this.y + "px";
   }
 
   isOutRight() {
     const screenSizeX = document.querySelector(".container").offsetWidth;
-    return this.x > (screenSizeX - playerImg.width); // is out if this.x > (1000-100)
+
+    return this.x > (screenSizeX - diverImg.width); // is out if this.x > (1000-100)
   }
+
+
+
 
   isOutLeft() {
     return this.x < 0;
@@ -27,58 +32,92 @@ class Diver {
   }
 
   isOutBottom() {
-    const screenSizeY = document.querySelector(".container").offsetHeight;
-    return this.y > (screenSizeY - playerImg.height); // is out if this.y > (600-60)
+    const screenSizeY = document.querySelector(".container").offsetHeight; // c'était .offsetHeight et pas juste .height !! to remember !!
+    return this.y > (screenSizeY - diverImg.height); // is out if this.y > (600-60)
   }
 
   move(keyState) { // se déplace en fonction de la touche appuyée
-    if (keyState['ArrowRight'] && !this.isOutRight()) { // quand bouge vers la droite: flip l'image horizontalement
-      console.log("in arrowRight");
+    if (keyState['ArrowRight'] && !this.isOutRight()) {
       this.domElement.style.transform = "scaleX(1)";
-      this.x++;
+      this.x += 3; // multiplie vitesse diver par 3 (au lieu de x++)
     }
     if (keyState['ArrowLeft'] && !this.isOutLeft()) { // quand bouge vers la gauche : flip l'image horizontallement
       this.domElement.style.transform = "scaleX(-1)"; // ajoute le flip
-      this.x--;
+      this.x -= 3;
     }
     if (keyState['ArrowUp'] && !this.isOutTop()) {
-      this.y--;
+      this.y -= 3;
     }
     if (keyState['ArrowDown'] && !this.isOutBottom()) {
-      this.y++;
+      this.y += 3;
     }
   }
-
-
 }
+
+//*******************************************************************/
 
 class Turtle {
-  constructor(x, y) {}
-  move() {}
-}
-
-class Trash {
-  constructor(x) {
-    // car à l'init. tombera sur l'axe x
+  constructor(x, y, domElement) {
+    this.x = x;
+    this.y = y;
+    this.domElement = domElement;
+    this.outLeft = false;
+    // this.node = document.createElement("img"); // créer mon image dans mon HTML
+    // document.getElementById("sea-screen").appendChild(this.node);
+    // permet de localiser mon image dans mon HTML (dans la div parent)
   }
-  fall() {}
+
+  updateInDom() {
+    this.domElement.style.left = this.x + "px";
+    this.domElement.style.top = this.y + "px";
+    // console.log("updating in DOM");
+  }
+
+  isOutRight() {
+    const screenSizeX = document.querySelector(".container").offsetWidth;
+    return this.x > (screenSizeX - turtleImg.width);
+  }
+
+  isOutLeft() {
+    return this.x < 0;
+  }
+  // pas besoin isOutTop and isOutLeft parce qu'elle va aller que de gauche à droite
+
+  moveLeft() {
+    if (this.x > 0 && !this.outLeft) {
+      this.x -= 2; // augmente sa vitesse ! = la déplace de 2x -
+      this.domElement.style.transform = "scaleX(1)";
+      // console.log("in move left");
+    }
+    if (this.x <= 0) this.outLeft = true;
+    // ne se déplace donc plus de -2 (vers la gauche) car outLeft devient true
+  }
+  moveRight() {
+    const screenSizeX = document.querySelector(".container").offsetWidth;
+    const width = this.domElement.getBoundingClientRect().width;
+    // A améliorer : ne pas répéter cette ligne isOutRight juste au dessus...
+
+    if (this.x + width < screenSizeX && this.outLeft) {
+      this.domElement.style.transform = "scaleX(-1)"; // turtle se retourne quand elle part à droite
+      this.x += 2;
+      console.log("in mv Right"); //et compteur stop après
+    }
+    if (this.x + width >= screenSizeX && this.outLeft) {
+      this.outLeft = false;
+      console.log("in mv Right and if > width");
+    }
+  }
 }
 
-// ***********************$
-// permet d'initialiser les instances = leur donner une vraie position sur le screen
-
-
-// console.log(diver.move(37)); // with 37, suppose to go left so x-1 = 2;
-// console.log(diver.x, diver.y); // => 2, 2
-// console.log(diver.move(38)); // => up, 2, 1
-// console.log(diver.x, diver.y);
-// console.log(diver.move(38)); // => up  2, 0
-// console.log(diver.x, diver.y);
-// console.log(diver.move(38)); // => up 2, -1
-// console.log(diver.isInsideMap()); // should return false
-// console.log(diver.move(40)); // => down 2, 0
-// console.log(diver.isInsideMap()); // should return true
 
 
 
-// *************************
+
+// class Trash {
+//   constructor(x) {
+//     // car à l'init. tombera sur l'axe x
+//   }
+//   fall() {}
+// }
+
+// ***********************
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
-const playerImg = document.getElementById("player");
-// console.log(player)
-const diver = new Diver(50, 10, playerImg);
-// const turtle1 = new Turtle(0, 0);
+const diverImg = document.getElementById("diver");
+const diver = new Diver(50, 10, diverImg);
+
+const turtleImg = document.getElementById("turtle");
+const turtle = new Turtle(800, 430, turtleImg);
+// turtleImg.getBoundingClientRect
 const trashes = []; // par défaut vide
 
 //************************************************
@@ -11,7 +13,6 @@ const keyState = {}; // renvoie
 
 window.onkeydown = function (e) { // quand touche est appuyée, le statut de la touche est "true"
     keyState[e.code] = true;
-    //console.log(keyState);
 };
 
 window.onkeyup = function (e) { // quand touche est relevée, le statut de la touche est "false"
@@ -19,9 +20,11 @@ window.onkeyup = function (e) { // quand touche est relevée, le statut de la to
 };
 
 function gameLoop() {
-    console.log("getting updated");
     diver.move(keyState); // checke le move à faire en fonction de la touche du clavier
-    diver.updateInDom(); // checke la position dans le DOM et update le CSS
+    diver.updateInDom();
+    turtle.moveLeft(); // first moveLeft
+    turtle.moveRight(); // then moveRight but stop loop after that
+    turtle.updateInDom(); // checke la position dans le DOM et update le CSS
     requestAnimationFrame(gameLoop); // s'appelle à elle-meme
 }
 requestAnimationFrame(gameLoop); //requestAnimationFrame demande au navigateur de faire appel à gameLoop (qui est une callback function)
