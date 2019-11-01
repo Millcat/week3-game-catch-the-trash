5d4f1ff361226f74f64d7cc932a065d42531ef9a
camille <camille.grison@gmail.com>
2019-11-01 09:59:43 +0100

    popUp win and loose added to the DOM and hasWin if score > 15

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,12 @@ const turtle = new Turtle(800, 430, turtleImg); // la mettre + bas pour qu'elle 
 
 const trashShoeImg = document.getElementById("shoe");
 
-const trashes = []; // par défaut vide et s'incrémentera avec les trash qui seront pushés dedans
+var trashes = []; // par défaut vide et s'incrémentera avec les trash qui seront pushés dedans
 
+// var popUpWinVisible = document.querySelector(".popUp.win").style.visibility;
+console.log("youWin" + document.querySelector(".popUp"));
+// var popUpLooseVisible = document.querySelector(".popUp.Loose").style.visibility;
+console.log("youLoose" + document.querySelector(".popUp"));
 //************************************************
 // Fluid move
 
@@ -25,10 +29,15 @@ window.onkeyup = function (e) { // quand touche est relevée, le statut de la to
 var count = 0;
 
 function gameLoop() {
+    let trashTouchedIndex; // déclare la variable qui permettra
+    let hasLost = false; // faux tant que trash n'a pas touché tortue
+    let hasWin = false; // faux tant que diver n'a pas catch 15 trash
+    // popUpWinVisible.innerHTML = "hidden";
+    // popUpLooseVisible.innerHTML = "hidden";
+
     diver.move(keyState); // checke le move à faire en fonction de la touche du clavier
 
     count++;
-
     if (count >= 240) { // 1 sec = 60 itérations ==> 4 sec = 240
         // permet d'ajouter toutes les 4 sec une image dans l'array
         trashes.push(new Trash(-30, "./images/trash_images/basket.png", "shoe", 2)); // vitesse x2
@@ -41,9 +50,6 @@ function gameLoop() {
     turtle.moveLeft(); // first moveLeft
     turtle.moveRight(); // then moveRight but stop loop after that
 
-    let trashTouchedIndex; // déclare la variable qui permettra
-    let hasLost = false; // faux tant que trash n'a pas touché tortue
-    // let hasWin = false; // faux tant que diver n'a pas catch 15 trash
 
     trashes.forEach((trash, i) => { // boucle sur mon array d'objets
         trash.fall(); // d'abord, tombe
@@ -54,32 +60,29 @@ function gameLoop() {
             document.querySelector(".container").removeChild(trash.domElement);
         }
 
-        if (diver.score == 15) { // si le diver a attrapé 15 objets
-            hasWin = true; // alors tu as gagné
-        }
-
         if (trash.touches(turtle.domElement)) { // si le trash touche la tortue
             hasLost = true; // alors tu as perdu
         }
     });
-
 
     if (trashTouchedIndex) { // remove le trash touché de l'array trash
         trashes.splice(trashTouchedIndex, 1);
     }
 
     if (hasLost) { // hasLost is false par defaut => continue la loop si on n'a pas perdu
-        // document.querySelector(".popUpLoose").classList.toggle("visible");
-
-
+        document.querySelector(".popUp.loose").classList.toggle("active")
+        console.log("you loose");
+    } else if (diver.score >= 15) { // si le diver a attrapé 15 objets
+        hasWin = true; // alors tu as gagné
+        document.querySelector(".popUp.win").classList.toggle("active")
+        console.log(hasWin);
     } else {
         requestAnimationFrame(gameLoop); // continue le jeu
     }
 
-    // if (hasWin) {
-    //     // @todo Afficher pop-up win
-    //     // document.querySelector(".popUpWin").classList.toggle("visible");
-    //     // == si la classe hidden apparait alors enlève là. sinon ajoute là
+    // if (hasWin) { // pourquoi fait planter le jeu
+    //     // popUpWinVisible.innerHTML = "visible";
+    //     console.log("you wiin");
     // } else {
     //     requestAnimationFrame(gameLoop); // continue le jeu
     // }
